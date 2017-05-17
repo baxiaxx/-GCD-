96b69217087c901dca510a1caf647aceea51880a
xiaozhenyangcode <xiaozheyangcode@163.com>
2017-05-17 17:29:16 +0800

    Initial Commit

---
+++ GCD (多线程)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCD (多线程)
+//
+//  Created by 一天 on 2017/5/17.
+//  Copyright © 2017年 肖振阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCD (多线程)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCD (多线程)
+//
+//  Created by 一天 on 2017/5/17.
+//  Copyright © 2017年 肖振阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
